f84e5e12b9b013433af1bf3295a40f08e86afdec
Pedro Henrique Morais Pereira <pedromorais3012@gmail.com>
2024-05-21 22:12:39 -0300

    Create signIn and signOut button using next-auth

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,10 @@
 import styles from "./styles.module.css";
 import Link from 'next/link'
+import { useSession, signIn, signOut } from 'next-auth/react'
 
 export function Header(){
+   const { data: session, status } = useSession()
+
    return(
       <header className={styles.header}>
          <section className={styles.content}>
@@ -14,7 +17,19 @@ export function Header(){
                <Link href="/dashboard" className={styles.dashboard}>Meu painel</Link>
             </nav>
 
-            <button className={styles.loginButton}>Acessar</button>
+            {status === 'loading' ? (
+               <></>
+            ) : session ? (
+               <button className={styles.loginButton} onClick={() => signOut()}>
+                  Olá, <u> {session?.user?.name} </u>
+               </button>
+            ): (
+               <button className={styles.loginButton} onClick={() => signIn("google")}>
+                  Acessar
+               </button>
+            )}
+
+            
          </section>
       </header>
    )

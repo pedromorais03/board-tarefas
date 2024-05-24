22325204025b7a0c27cb10d2d19a22fb119d8db7
Pedro Henrique Morais Pereira <pedromorais3012@gmail.com>
2024-05-23 22:18:37 -0300

    Marked-up /dashboard page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,8 +1,11 @@
 import { GetServerSideProps } from 'next'
+import { getSession } from 'next-auth/react'
 import styles from './styles.module.css'
 import Head from 'next/head'
 
-import { getSession } from 'next-auth/react'
+import { Textarea } from '../../components/textarea'
+import {FiShare2} from 'react-icons/fi'
+import {FaTrash} from 'react-icons/fa'
 
 export default function Dashboard(){
    
@@ -12,7 +15,44 @@ export default function Dashboard(){
             <title> Meu painel de tarefas </title>
          </Head>
 
-         <h1>Pg painel</h1>
+         <main className={styles.main}>
+            <section className={styles.content}>
+               <div className={styles.contentForm}>
+                  <h1 className={styles.title}>Qual sua tarefa?</h1>
+                  
+                  <form>
+                     <Textarea placeholder='Digite sua tarefa' />
+                     <div className={styles.checkboxArea}>
+                        <input className={styles.checkbox} type="checkbox" />
+                        <label>Deixar tarefa pública</label>
+                     </div>
+
+                     <button  type="submit" className={styles.button}>Registrar</button>
+                  </form>
+               </div>
+            </section>
+
+            <section className={styles.taskContainer}>   
+               <h1>Minhas tarefas</h1>
+
+               <article className={styles.task}>
+                  <div className={styles.tagContainer}>
+                     <label className={styles.tag}>PÚBLICO</label>
+                     <button className={styles.shareButton}>
+                        <FiShare2 size={22} color="#3183ff" />
+                     </button>
+                  </div>
+                  <div className={styles.taskContent}>
+                     <p>
+                        Minha task
+                     </p>
+                     <button className={styles.trashButton}>
+                        <FaTrash size={24} color="#ea3140" />
+                     </button>
+                  </div>
+               </article>
+            </section>
+         </main>
       </div>
    )
 }
@@ -34,7 +74,7 @@ export const getServerSideProps : GetServerSideProps = async ({req}) => {
 
    return {
       props: {
-         
+
       }
    }
 }
